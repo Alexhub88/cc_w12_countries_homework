9dd2a0f060f8df54840170dca4328ec4ac4a8462
Alexhub88 <alexscott88@hotmail.co.uk>
2018-05-26 16:59:26 +0100

    Commit 2: select dropdowns for filter categories working.

--- src/views/countries_view.js
+++ src/views/countries_view.js
@@ -11,13 +11,22 @@ CountriesView.prototype.bindEvents = function () {
 }
 
 CountriesView.prototype.render = function (filters) {
+
   filters.forEach((filter) => {
     const filterCategory = document.createElement('p');
     filterCategory.textContent = filter.filterName;
     this.container.appendChild(filterCategory);
+
+    const select = document.createElement('select')
+    filter.subfilters.forEach((subfilter, index) => {
+      const option = document.createElement('option');
+      option.textContent = subfilter;
+      option.value = index;
+      select.appendChild(option);
+    })
+    this.container.appendChild(select);
   })
 }
-
 
 
 module.exports = CountriesView;
